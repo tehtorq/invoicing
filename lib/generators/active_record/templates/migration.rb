c18eff31e5c5e9abce286521a74bd7cf479a9c6f
Douglas Anderson <i.am.douglas.anderson@gmail.com>
2012-03-01 13:07:15 +0200

    float -> decimal columns

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -3,32 +3,32 @@ class CreateInvoicingTables < ActiveRecord::Migration
 
     create_table "invoicing_late_payments", :force => true do |t|
       t.integer  "invoice_id"
-      t.float "amount"
+      t.decimal "amount", precision: 10, scale: 2
       t.datetime "penalty_date"
       t.boolean  "processed"
       t.timestamps
     end
-    
+
     create_table "invoicing_line_items", :force => true do |t|
       t.integer "invoice_id"
       t.string  "description"
-      t.float   "amount"
+      t.decimal   "amount", precision: 10, scale: 2
       t.timestamps
     end
-    
+
     create_table "invoicing_transactions", :force => true do |t|
       t.integer "invoice_id"
       t.string  "type"
-      t.float   "amount"
+      t.decimal "amount", precision: 10, scale: 2
       t.timestamps
     end
-    
+
     create_table "invoicing_invoices", :force => true do |t|
       t.string   "invoice_number"
       t.datetime "due_date"
-      t.float    "total"
-      t.float    "vat_amount"
-      t.float    "balance"
+      t.decimal  "total", precision: 10, scale: 2
+      t.decimal  "vat_amount", precision: 10, scale: 2
+      t.decimal  "balance", precision: 10, scale: 2
       t.timestamps
     end
  
